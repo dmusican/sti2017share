7df9107a48176e95fcd6765102a2a0d629e14f6b
Dave Musicant on Skittles <meOnSkittles@carleton.edu>
2017-06-26 15:57:32 -0500

    Scripted auto-commit on change (2017-06-26 15:57:32) by gitwatch.sh

--- warmups/SlowSorts.java
+++ warmups/SlowSorts.java
@@ -2,7 +2,7 @@ public class SlowSorts {
    public static void main(String[] args) {
       int[] array = new int[5];
       for (int i=0; i < array.length; i++) {
-         array[i] = Math.random()*10000;
+         array[i] = (int)(Math.random()*10000;)
       }
       
       for (int i=0; i < array.length; i++) {
