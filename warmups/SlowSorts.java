7f39faf884f5e67b050968b93affcf4a0cc927b7
Dave Musicant on Skittles <meOnSkittles@carleton.edu>
2017-06-26 16:23:13 -0500

    Scripted auto-commit on change (2017-06-26 16:23:12) by gitwatch.sh

--- warmups/SlowSorts.java
+++ warmups/SlowSorts.java
@@ -1,18 +1,38 @@
 public class SlowSorts {
+   public static void swap(int[] values, int i, int j) {
+      int temp = values[i];
+      values[i] = values[j];
+      values[j] = temp;
+   }
+      
+   
+   public static void bubbleSort(int[] array) {
+      for (int i=0; i < array.length - 1; i++) { 
+         for (int j = 0; j < array.length - 1 - i; j++) {
+            if (array[j] > array[j+1]) {
+               swap(array, j, j+1);
+            }
+         }
+      }
+   }
+
+
+
    public static void main(String[] args) {
       int[] array = new int[5];
+      
+      // Generate random values
       for (int i=0; i < array.length; i++) {
          array[i] = (int)(Math.random()*10000);
       }
+    
+      bubbleSort(array);
       
+      // Display values  
       for (int element : array) {
          System.out.print(element + " ");
       }
-      
-      for (int i=0; i < 5; i++) {
-         System.out.println();
-      }
-      
+       
       System.out.println();
    }
 }
