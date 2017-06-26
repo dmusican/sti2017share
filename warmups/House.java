5c321d8bcc7a4e2eb3d384cd7e8e6b4d4433bced
Dave Musicant on Skittles <meOnSkittles@carleton.edu>
2017-06-26 13:28:31 -0500

    Scripted auto-commit on change (2017-06-26 13:28:31) by gitwatch.sh

--- warmups/House.java
+++ warmups/House.java
@@ -3,8 +3,10 @@ public class House
   
   public static void main(String[] args)
   {
-    Canvas houseCanvas;
-    houseCanvas = new Canvas();
+    Canvas houseCanvas;                  // declaration
+    houseCanvas = new Canvas();          // object creation
+    houseCanvas.setSize(100,100);
+    houseCanvas.setVisible(true);
   } 
   
 }
