e9066f9f6e0c915939480e4885c2ddcc92fbb194
Dave Musicant on Skittles <meOnSkittles@carleton.edu>
2017-06-28 08:57:37 -0500

    Scripted auto-commit on change (2017-06-28 08:57:37) by gitwatch.sh

---
+++ AI.java
@@ -0,0 +1,9 @@
+public class AI implements Player {
+   public int drawSticks() {
+      return 1;
+   }
+   
+   public void summarize() {
+      System.out.println("The AI has vanquished you!");
+   }
+}
