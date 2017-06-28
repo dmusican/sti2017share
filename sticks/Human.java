038bdc4ff2f4125d92e31e73d3ea3dd65a3179c8
Dave Musicant on Skittles <meOnSkittles@carleton.edu>
2017-06-28 09:02:41 -0500

    Scripted auto-commit on change (2017-06-28 09:02:40) by gitwatch.sh

--- sticks/Human.java
+++ sticks/Human.java
@@ -1,13 +1,21 @@
 import java.util.Scanner;
 
 public class Human implements Player {
+   private int playerNumber;
+   
+   public Human(int number) {
+      playerNumber = number;
+   }
+
    public int drawSticks() {
-      System.out.print("Player, how many sticks?");
+      System.out.print("Player " + playerNumber +
+                       ", how many sticks?");
       Scanner input = new Scanner(System.in);
       return input.nextInt();
    }
    
    public void summarize() {
-      System.out.println("Player, you win!");
+      System.out.println("Player " + playerNumber + 
+                         ", you win!");
    }
 }
--- sticks/Sticks2.java
+++ sticks/Sticks2.java
@@ -6,7 +6,7 @@ public class Sticks2 {
       int turn = 1;
       Scanner input = new Scanner(System.in);
 
-      Player player1 = new Human();
+      Player player1 = new Human(1);
       Player player2 = null;
             
       System.out.println("Against computer or human? ");
@@ -14,7 +14,7 @@ public class Sticks2 {
       if (response.equals("computer")) {
          player2 = new AI();
       } else {
-         player2 = new Human();
+         player2 = new Human(2);
       }
       
       Player currentPlayer = player1;
