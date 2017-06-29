037463d97226d8b03096dec29cb03aa9b37ecc55
Dave Musicant on Skittles <meOnSkittles@carleton.edu>
2017-06-29 09:31:55 -0500

    Scripted auto-commit on change (2017-06-29 09:31:54) by gitwatch.sh

---
+++ recursion/Examples.java
@@ -0,0 +1,22 @@
+public class Examples {
+   
+   // 1. Base case
+   // 2. Recursive step
+ 
+   public static int factorial(int n) {
+
+      if (n == 1) {
+         return 1;  
+      } else {
+         return n * factorial(n-1);
+      }
+      
+   
+   
+   }
+   
+   public static void main(String[] args) {
+   
+      System.out.println(factorial(5));
+   }
+}
