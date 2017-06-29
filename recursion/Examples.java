078121a648ab747e3d17a024720fb0b4061176b5
Dave Musicant on Skittles <meOnSkittles@carleton.edu>
2017-06-29 10:07:51 -0500

    Scripted auto-commit on change (2017-06-29 10:07:51) by gitwatch.sh

--- recursion/Examples.java
+++ recursion/Examples.java
@@ -19,15 +19,24 @@ public class Examples {
          return s;
       } else {
          return reverseString(s.substring(1)) + s.substring(0,1);
-      }
-   
-   
+      }   
    }
+   
+   // Iteratively do exponentation
+   public int iterativeExp(int base, int power) {
+      int answer = 1;
+      for (int i=0; i < power; i++) {
+         answer = answer * base;
+      }
+      return answer;
+   }
+   
    
    public static void main(String[] args) {
    
       System.out.println(factorial(3));
       System.out.println(reverseString("friend"));
+      System.out.println(interativeExp(2,5));
       
    }
 }
